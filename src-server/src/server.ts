26f48cbc998ab8737ac4d62509d67d51db522a11
slabkiy <slabkiy@skbkontur.ru>
2024-09-01 21:01:05 +0300

    init

---
+++ src-server/src/server.ts
@@ -0,0 +1,11 @@
+﻿import express, { Express } from "express";
+import {router as v1} from "./v1/routes";
+
+export const app: Express = express();
+const port = 4029;
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+});
+
+app.use('/api/v1', v1);
---
+++ src-server/src/v1/routes/index.ts
@@ -0,0 +1,11 @@
+﻿import { Router } from "express";
+
+export const router = Router();
+
+router.route('/query').get((req, res) => {
+  res.send({
+    id: '1',
+    name: 'John Doe',
+    email: 'johndoe@example.com'
+  })
+});
